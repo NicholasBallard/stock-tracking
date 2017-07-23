f369641f99488451576cd1650ef051269ab953cb
nicholasericballard <nicholasericballard@gmail.com>
2017-07-23 12:36:16 -0400

    adding historical function to retrieve and write date ranges to tracking file

---
+++ get-historical.py
@@ -0,0 +1,23 @@
+# get-historical.py
+'''
+Import this function into app.py
+Use it to write the daily p/l for the portfolio by day \
+for a date range.
+'''
+
+from yahoo-finance import Share
+import datetime
+import pandas as pd
+
+def history( stock_portfolio , start_date , end_date ):
+  date = start_date
+  while date <= end_date:
+    day_change = 0
+    for company in portfolio:
+      close = Share(company).get_price()
+      previous = Share(company).get_prev_close()
+      change = close - previous
+      day_change += change
+    # TODO: Write `day_change` to price-tracker.txt as `date  , day_change r'\n`
+    # date.strftime("%m-%d-%Y") + "\t,\t" + day_change
+    date += 1
